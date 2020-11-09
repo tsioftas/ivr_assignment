8c58e09051c4d57267a015eca913c4737fb41e06
tsioftas <panaevri@gmail.com>
2020-11-09 16:10:25 +0000

    Linting

--- src/coordinates_extractor.py
+++ src/coordinates_extractor.py
@@ -1,40 +1,40 @@
 #!/usr/bin/env python3
 
-import sys
 import cv2
 import numpy as np
 
 # Class used to extract object coordinates from images using color thresholding
 class coordinates_extractor:
 
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  # Returns a binary image of a colored region in an image using thresholding
-  # color_thresholds has shape (2,3)
-  # color_thresholds[0,i] = min value for color i
-  # color_thresholds[1,i] = max value for color i
-  def get_blob_threshold(self, img, color_thresholds):
-    assert color_thresholds.shape == (2,3)
-    return cv2.inRange(img, color_thresholds[0,:], color_thresholds[1,:])
+    # Returns a binary image of a colored region in an image using thresholding
+    # color_thresholds has shape (2,3)
+    # color_thresholds[0,i] = min value for color i
+    # color_thresholds[1,i] = max value for color i
+    def get_blob_threshold(self, img, color_thresholds):
+        assert color_thresholds.shape == (2, 3)
+        return cv2.inRange(img, color_thresholds[0, :], color_thresholds[1, :])
 
-  # Given a binary image of a blob calculates its centre in pixel coordinates
-  def get_blob_coordinates(self, blob):
-    # Apply dilate to make the region larger
-    kernel = np.ones((5,5), np.uint8)
-    blob = cv2.dilate(blob, kernel, iterations=3)
-    # Use moments to calculate centre of the blob
-    M = cv2.moments(blob)
-    cx = int(M['m10'] / M['m00'])
-    cy = int(M['m01'] / M['m00'])
-    return np.array([cx, cy])
+    # Given a binary image of a blob calculates its centre in pixel coordinates
+    def get_blob_coordinates(self, blob):
+        # Apply dilate to make the region larger
+        kernel = np.ones((5, 5), np.uint8)
+        blob = cv2.dilate(blob, kernel, iterations=3)
+        # Use moments to calculate centre of the blob
+        M = cv2.moments(blob)
+        cx = int(M['m10'] / M['m00'])
+        cy = int(M['m01'] / M['m00'])
+        return np.array([cx, cy])
 
 # test the class
-def main(args):
-  ce = coordinates_extractor()
-  red_thresholds = np.array([[0,0,100], [10,10,255]])
-  img = cv2.imread('image_copy.png', cv2.IMREAD_COLOR)
-  print(ce.get_coordinates(img, red_thresholds))
+def main():
+    ce = coordinates_extractor()
+    red_thresholds = np.array([[0, 0, 100], [10, 10, 255]])
+    img = cv2.imread('image_copy.png', cv2.IMREAD_COLOR)
+    blob = ce.get_blob_threshold(img, red_thresholds)
+    print(ce.get_blob_coordinates(blob))
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
--- src/joints_locator.py
+++ src/joints_locator.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
 import cv2
 import numpy as np
 
@@ -10,63 +9,63 @@ from coordinates_extractor import coordinates_extractor
 # Uses coordinates_extractor to get positions of joints in an image
 class joints_locator:
 
-  def __init__(self):
-    self.blue_thresholds = np.array([[100,0,0], [255,10,10]])
-    self.green_thresholds = np.array([[0,100,0], [10,255,10]])
-    self.red_thresholds = np.array([[0,0,100], [10,10,255]])
-    self.yellow_thresholds = np.array([[0,100,100], [10,255,255]])
-    self.ce = coordinates_extractor()
-    pass
+    def __init__(self):
+        self.blue_thresholds = np.array([[100, 0, 0], [255, 10, 10]])
+        self.green_thresholds = np.array([[0, 100, 0], [10, 255, 10]])
+        self.red_thresholds = np.array([[0, 0, 100], [10, 10, 255]])
+        self.yellow_thresholds = np.array([[0, 100, 100], [10, 255, 255]])
+        self.ce = coordinates_extractor()
 
-  def get_blue_joint(self, img):
-    blob = self.ce.get_blob_threshold(img, self.blue_thresholds)
-    return self.ce.get_blob_coordinates(blob)
+    def get_blue_joint(self, img):
+        blob = self.ce.get_blob_threshold(img, self.blue_thresholds)
+        return self.ce.get_blob_coordinates(blob)
 
-  def get_green_joint(self, img):
-    blob = self.ce.get_blob_threshold(img, self.green_thresholds)
-    return self.ce.get_blob_coordinates(blob)
+    def get_green_joint(self, img):
+        blob = self.ce.get_blob_threshold(img, self.green_thresholds)
+        return self.ce.get_blob_coordinates(blob)
 
-  def get_red_joint(self, img):
-    blob = self.ce.get_blob_threshold(img, self.red_thresholds)
-    return self.ce.get_blob_coordinates(blob)
-  
-  def get_yellow_joint(self, img):
-    blob = self.ce.get_blob_threshold(img, self.yellow_thresholds)
-    # separate yellow from orange
-    bin2 = img.copy()
-    bin2[img[:,:,1] == img[:,:,2]] = 255
-    bin2[img[:,:,1] != img[:,:,2]] = 0
-    blob = blob & bin2[:,:,0]
-    return self.ce.get_blob_coordinates(blob)
+    def get_red_joint(self, img):
+        blob = self.ce.get_blob_threshold(img, self.red_thresholds)
+        return self.ce.get_blob_coordinates(blob)
 
-  def get_joints_pixel_location(self, img):
-    y = self.get_yellow_joint(img)
-    b = self.get_blue_joint(img)
-    g = self.get_green_joint(img)
-    r = self.get_red_joint(img)
-    # Origin at the yellow joint
-    ret = np.array([y,b,g,r])
-    ret -= y
-    # Flip y axis (make up positive)
-    ret[:,1] *= -1
-    return ret
+    def get_yellow_joint(self, img):
+        blob = self.ce.get_blob_threshold(img, self.yellow_thresholds)
+        # separate yellow from orange
+        bin2 = img.copy()
+        bin2[img[:, :, 1] == img[:, :, 2]] = 255
+        bin2[img[:, :, 1] != img[:, :, 2]] = 0
+        blob = blob & bin2[:, :, 0]
+        return self.ce.get_blob_coordinates(blob)
 
-  # Calculates (relative) xyz coordinates of joints, with a frame of reference as shown in figure 1 of the specifications document.
-  # img_yz: image from camera1
-  # img_xz: image from camera2
-  def get_joints_xyz_locations(self, img_yz, img_xz):
-    loc1 = self.get_joints_pixel_location(img_yz)
-    loc2 = self.get_joints_pixel_location(img_xz)
-    real_x = loc2[:,0]
-    real_y = loc1[:,0]
-    real_z = (loc1[:,1] + loc2[:,1])/2
-    return np.array([real_x, real_y, real_z])
+    def get_joints_pixel_location(self, img):
+        y = self.get_yellow_joint(img)
+        b = self.get_blue_joint(img)
+        g = self.get_green_joint(img)
+        r = self.get_red_joint(img)
+        # Origin at the yellow joint
+        ret = np.array([y, b, g, r])
+        ret -= y
+        # Flip y axis (make up positive)
+        ret[:, 1] *= -1
+        return ret
+
+    # Calculates (relative) xyz coordinates of joints, with a frame of reference as shown
+    # in figure 1 of the specifications document.
+    # img_yz: image from camera1
+    # img_xz: image from camera2
+    def get_joints_xyz_locations(self, img_yz, img_xz):
+        loc1 = self.get_joints_pixel_location(img_yz)
+        loc2 = self.get_joints_pixel_location(img_xz)
+        real_x = loc2[:, 0]
+        real_y = loc1[:, 0]
+        real_z = (loc1[:, 1] + loc2[:, 1])/2
+        return np.array([real_x, real_y, real_z])
 
 # test the class
-def main(args):
-  jl = joints_locator()
-  img = cv2.imread('image_copy.png', cv2.IMREAD_COLOR)
-  print(jl.get_joints_pixel_location(img))
+def main():
+    jl = joints_locator()
+    img = cv2.imread('image_copy.png', cv2.IMREAD_COLOR)
+    print(jl.get_joints_pixel_location(img))
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
