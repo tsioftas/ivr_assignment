3abc00438552b02537f92e0b233e1a78299e1355
tsioftas <panaevri@gmail.com>
2020-11-25 17:54:24 +0200

    Tidy target_image_sampler.py

--- src/target_image_sampler.py
+++ src/target_image_sampler.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-import sys
 import rospy
 import cv2
 import numpy as np
@@ -13,6 +12,7 @@ from target_locator import target_locator
 import constants
 
 class target_image_sampler:
+    """Class used to take image samples of the target sphere and the cuboid"""
 
     def __init__(self):
         # initialize the node
@@ -21,9 +21,15 @@ class target_image_sampler:
         self.image_sub1 = rospy.Subscriber("/camera1/robot/image_raw", Image, self.callback_img1)
         self.image_sub2 = rospy.Subscriber("/camera2/robot/image_raw", Image, self.callback_img2)
         # initialize subscribers to receive actual position of target
-        self.target_x_sub = rospy.Subscriber("/target/x_position_controller/command", Float64, self.callback_x)
-        self.target_y_sub = rospy.Subscriber("/target/y_position_controller/command", Float64, self.callback_y)
-        self.target_z_sub = rospy.Subscriber("/target/z_position_controller/command", Float64, self.callback_z)
+        self.target_x_sub = rospy.Subscriber("/target/x_position_controller/command",
+                                             Float64,
+                                             self.callback_x)
+        self.target_y_sub = rospy.Subscriber("/target/y_position_controller/command",
+                                             Float64,
+                                             self.callback_y)
+        self.target_z_sub = rospy.Subscriber("/target/z_position_controller/command",
+                                             Float64,
+                                             self.callback_z)
         # initialize the bridge between openCV and ROS
         self.bridge = CvBridge()
         # initialize target locator
@@ -40,21 +46,24 @@ class target_image_sampler:
         self.is_updated_z = False
         self.is_updated_img1 = False
         self.is_updated_img2 = False
-        # initialize samples dimensions
+        # initialize samples' dimensions
         self.sample_w = 30
         self.sample_h = 30
-        # initialize samples count and limit
+        # initialize samples count per class and limit
         self.samples_count = 0
         self.samples_limit = 10000
-        # initialize save directory
+        # initialize save directory (directory works if ran from catkin_ws)
         self.save_dir = constants.ML_DATA_DIR
 
 
+    # Checks that coordinate data and image data has been updated
     def data_updated(self):
         coords_updated = self.is_updated_x and self.is_updated_y and self.is_updated_z
         images_updated = self.is_updated_img1 and self.is_updated_img2
         return coords_updated and images_updated
-    
+
+
+    # Resets data updated status
     def reset_data(self):
         self.is_updated_x = False
         self.is_updated_y = False
@@ -62,11 +71,15 @@ class target_image_sampler:
         self.is_updated_img1 = False
         self.is_updated_img2 = False
 
+
+    # Checks if all data has been updated and if so takes samples
     def take_action_if_updated(self):
-        if(self.data_updated()):
+        if self.data_updated():
             self.reset_data()
             self.take_samples()
 
+
+    # Callbacks for receiving data from topics
     def callback_x(self, data):
         self.target_x = data.data
         self.is_updated_x = True
@@ -102,6 +115,10 @@ class target_image_sampler:
             print(e)
         self.take_action_if_updated()
 
+
+    # Increases the samples count and prints it every 100 samples to monitor progress.
+    # When the samples count reaches the limit it is not increased, and a relevant
+    # message is shown.
     def increase_samples_count(self):
         if self.samples_count + 1 < self.samples_limit:
             self.samples_count += 1
@@ -110,6 +127,8 @@ class target_image_sampler:
         else:
             print("DONE!")
 
+
+    # Generates names (and paths) for new samples and increases the samples count.
     def generate_image_names(self):
         path = self.save_dir
         num = str(self.samples_count)
@@ -118,79 +137,83 @@ class target_image_sampler:
         cuboid_path = path+'cuboid_'+num+'.png'
         return (sphere_path, cuboid_path)
 
+
+    # Takes samples for each image and saves them. If no sample can be taken from an image
+    # (either because the two objects overlap or at least one of them is not visible)
+    # no sample is saved.
     def take_samples(self):
-        if(not self.data_updated):
+        if not self.data_updated:
             return
         img1 = self.img1
         img2 = self.img2
         # take samples from img1
         try:
             sph, cub = self.take_sample_from(img1, self.target_y, self.target_z)
-            sph_name , cub_name = self.generate_image_names()
+            sph_name, cub_name = self.generate_image_names()
             cv2.imwrite(sph_name, sph)
             cv2.imwrite(cub_name, cub)
-        except Exception as e:
+        except Exception as _:
             pass
         # take samples from img2
         try:
             sph, cub = self.take_sample_from(img2, self.target_x, self.target_z)
-            sph_name , cub_name = self.generate_image_names()
+            sph_name, cub_name = self.generate_image_names()
             cv2.imwrite(sph_name, sph)
             cv2.imwrite(cub_name, cub)
-        except Exception as e:
-            print(e)
+        except Exception as _:
+            pass
 
-        
+
+    # Given the (real) x and y coordinates of the sphere in img,
+    # returns two crops of img the first one containing the sphere and
+    # the second one containing the cuboid. Returns None if no sample can be taken
     def take_sample_from(self, img, x, y):
+        # Get blob binary image and coordinates of blobs using target_locator
         blobs = self.tl.get_orange_blobs_img(img)
         blobs_centres = self.tl.get_orange_blobs_centres(img)
-        
-        if blobs_centres.shape != (2,2):
-            # shapes are overlapping, don't take sample
-            return None
-        xy_actual = np.array([x, y]) * constants.get_meters_to_pixels_coefficient()
-        xy_blob1 = blobs_centres[0,:]
-        xy_blob2 = blobs_centres[1,:]
 
+        if blobs_centres.shape != (2, 2):
+            # Shapes are overlapping / not visible, don't take a sample.
+            return None
+        # Convert x,y coordinates of sphere from meters to pixels
+        xy_actual = np.array([x, y]) * constants.get_meters_to_pixels_coefficient()
+        xy_blob1 = blobs_centres[0, :]
+        xy_blob2 = blobs_centres[1, :]
+        # Get distance of each blob from actual sphere position
         d1 = np.linalg.norm((xy_actual-xy_blob1))
         d2 = np.linalg.norm((xy_actual-xy_blob2))
-
-        if d1 > d2:
-            # sphere is blob2
-            sph = 1
-            cub = 0
-        else:
-            sph = 0
-            cub = 1
-        c_sph = blobs_centres[sph,:]
-        c_cub = blobs_centres[cub,:]
+        # The sphere is the blob with with the smallest distance
+        sph, cub = (1, 0) if d1 > d2 else (0, 1)
+        c_sph = blobs_centres[sph, :]
+        c_cub = blobs_centres[cub, :]
+        # Take samples
         centres = [c_sph, c_cub]
         w = self.sample_w
         h = self.sample_h
-        ret = np.zeros((2,h,w))
+        ret = np.zeros((2, h, w))
         for i in range(2):
             c = centres[i]
             ul_x = c[0]-w//2
             ul_y = c[1]-h//2
             lr_x = ul_x + w
             lr_y = ul_y + h
-            ret[i,:,:] = blobs[ul_y:lr_y, ul_x:lr_x]
-        cv2.imshow('Sphere',ret[0,:,:])
-        cv2.imshow('Cuboid', ret[1,:,:])
-        cv2.waitKey(1)
-        return (ret[0,:,:], ret[1,:,:])
+            ret[i, :, :] = blobs[ul_y:lr_y, ul_x:lr_x]
+        # Uncomment to monitor samples as they are taken
+        # cv2.imshow('Sphere',ret[0,:,:])
+        # cv2.imshow('Cuboid', ret[1,:,:])
+        # cv2.waitKey(1)
+        return (ret[0, :, :], ret[1, :, :])
 
 
 
-# call the class
-def main(args):
-    tis = target_image_sampler()
+# call the class to take samples
+def main():
+    _ = target_image_sampler()
     try:
         rospy.spin()
     except KeyboardInterrupt:
         print("Shutting down")
     cv2.destroyAllWindows()
 
-# run the code if the node is called
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
