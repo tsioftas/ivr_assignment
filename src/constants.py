aa8fa3ab2e57c71c6475097f5aa10884a083d189
tsioftas <panaevri@gmail.com>
2020-11-25 13:05:17 +0200

    Refactoring, tidying and documentation of constants.py

--- src/constants.py
+++ src/constants.py
@@ -1,14 +1,13 @@
 import cv2 as cv
 import numpy as np
 
-
 # Local imports
 import joints_locator
 
 # Link lengths in meters
 LINK_L = [2.5, 0, 3.5, 3]
 
-# Initial images path
+# Initial images path (assumes running from catkin_ws)
 IMG1_INIT = './src/ivr_assignment/images/image1_init.png'
 IMG2_INIT = './src/ivr_assignment/images/image2_init.png'
 ML_DATA_DIR = './src/ivr_assignment/images/ml_samples/'
@@ -16,19 +15,24 @@ ML_DATA_DIR = './src/ivr_assignment/images/ml_samples/'
 # To be calculated during initialization
 PIXELS_TO_METERS_COEFFICIENT = None
 
-def get_link_l(x):
+def get_link_length(x):
+    """Returns the length (in meters) of the specified link. For example, get_link_l(1) returns 2.5"""
     return LINK_L[x-1]
 
 def get_pixels_to_meters_coefficient():
+    """Safe function used to get the coefficient for converting image pixels to meters"""
     global PIXELS_TO_METERS_COEFFICIENT
     if PIXELS_TO_METERS_COEFFICIENT is None:
+        # Calculate the coefficient from the sample images
         img1 = cv.imread(IMG1_INIT, cv.IMREAD_COLOR)
         img2 = cv.imread(IMG2_INIT, cv.IMREAD_COLOR)
         jl = joints_locator.joints_locator()
         locs = jl.get_joints_xyz_locations(img1, img2)
+        # z-coordinate of red joint
         r_z = locs[3,2]
         PIXELS_TO_METERS_COEFFICIENT = np.sum(LINK_L) / r_z
     return PIXELS_TO_METERS_COEFFICIENT
 
 def get_meters_to_pixels_coefficient():
+    """Safe function used to get the coefficient for converting meters to image pixels"""
     return 1/get_pixels_to_meters_coefficient()
